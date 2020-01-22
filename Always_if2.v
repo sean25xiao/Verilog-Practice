693e402874e811b465f500dd6de2525d28a8987d
Yibang Xiao <yibangxiao@gmail.com>
2020-01-21 23:19:18 -0500

    1/21: Exercises

---
+++ Always_if2.v
@@ -0,0 +1,43 @@
+// This code has latch bug!!!!!! fix it!!
+module top_module (
+    input      cpu_overheated,
+    output reg shut_off_computer,
+    input      arrived,
+    input      gas_tank_empty,
+    output reg keep_driving  ); //
+
+    always @(*) begin
+        if (cpu_overheated)
+           shut_off_computer = 1;
+    end
+
+    always @(*) begin
+        if (~arrived)
+           keep_driving = ~gas_tank_empty;
+    end
+
+endmodule
+
+//The fixed code is here:
+module top_module (
+    input      cpu_overheated,
+    output reg shut_off_computer,
+    input      arrived,
+    input      gas_tank_empty,
+    output reg keep_driving  ); //
+
+    always @(*) begin
+        if (cpu_overheated)
+           shut_off_computer = 1;
+        else
+           shut_off_computer = 0;
+    end
+
+    always @(*) begin
+        if (~arrived)
+           keep_driving = ~gas_tank_empty;
+        else
+           keep_driving = 0;
+    end
+
+endmodule
